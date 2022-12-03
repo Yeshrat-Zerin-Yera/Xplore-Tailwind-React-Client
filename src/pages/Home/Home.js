90420993d33103d9eed85c5f63b64bf48c451c4a
unknown <yeshratzerinyera@gmail.com>
2022-12-04 01:43:00 +0600

    Home Added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Banner from '../../assets/banner.jpg';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="hero h-[60vh]" style={{ backgroundImage: `url(${Banner})` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <h1 className="mb-5 text-4xl md:text-6xl font-bold">Your Bright Future Is Our Mission</h1>
+            </div>
         </div>
     );
 };
