499af307540acb09d80aa7ed91a18bf657354d1e
unknown <yeshratzerinyera@gmail.com>
2022-12-03 21:40:47 +0600

    Components Added

---
+++ src/pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-gray-600 text-center py-6'>Copyright © 2022 Xplore. All Rights Reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
