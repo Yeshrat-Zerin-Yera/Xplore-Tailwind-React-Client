adf7e3822098b03e987cabf1fecda170806d8fee
unknown <yeshratzerinyera@gmail.com>
2022-12-03 21:53:45 +0600

    Router Added

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../layouts/Main/Main';
+import Courses from '../../pages/Courses/Courses';
+import FAQ from '../../pages/FAQ/FAQ';
+import Home from '../../pages/Home/Home';
+import Login from '../../pages/SignUpAndLogin/Login/Login';
+import SignUp from '../../pages/SignUpAndLogin/SignUp/SignUp';
 
-const Routes = () => {
-    return (
-        <div>
-
-        </div>
-    );
-};
-
-export default Routes;
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            }
+        ]
+    }
+]);
