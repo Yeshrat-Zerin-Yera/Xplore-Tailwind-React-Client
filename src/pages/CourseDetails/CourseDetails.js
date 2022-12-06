0c43f4ed0f30292ac8ca568c9da7183d999cc4ec
unknown <yeshratzerinyera@gmail.com>
2022-12-06 19:30:07 +0600

    Download PDF Handled

--- src/pages/CourseDetails/CourseDetails.js
+++ src/pages/CourseDetails/CourseDetails.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import toast from 'react-hot-toast';
 import { FaArrowRight, FaBook, FaDownload, FaEye, FaLaptop, FaUser } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
+import { useReactToPrint } from 'react-to-print';
 import StarRated from '../Others/StarRated/StarRated';
 
 const CourseDetails = () => {
     const course = useLoaderData();
+    const componentRef = useRef();
     const { title, rating, introduction, thumbnail_url, others_info } = course;
     const courses = [];
 
+    // Get Course Button Action
     const handleGetCourse = () => {
         courses.push(course);
         const selectedCourses = localStorage.getItem('courses');
@@ -22,13 +26,21 @@ const CourseDetails = () => {
         }
     }
 
+    // Download PDF Button Action
+    const handleDownloadPDF = useReactToPrint({
+        content: () => componentRef.current,
+        documentTitle: 'course_data',
+        onAfterPrint: () => toast.success('PDF Download Successfull')
+    });
+
     return (
         // Course Details
         <div className='grid lg:grid-cols-12 gap-6 p-6 w-full md:w-11/12 lg:w-11/12 xl:w-8/12 mx-auto'>
             {/* Details Content */}
-            <div className='lg:col-span-7'>
+            <div ref={componentRef} className='lg:col-span-7 p-3'>
                 <h1 className='text-3xl sm:text-4xl lg:text-5xl text-primary font-bold my-6'>{title.toUpperCase()}</h1>
-                <button className='btn btn-primary btn-outline'>Get PDF<FaDownload className='ml-3' /></button>
+                {/* PDF Download Button */}
+                <button onClick={handleDownloadPDF} className='btn btn-primary btn-outline'>Get PDF<FaDownload className='ml-3' /></button>
                 <img src={thumbnail_url} alt="" className='w-full rounded my-6' />
                 <h2 className='text-3xl md:text-4xl text-primary font-bold mt-12 mb-6'>Introduction</h2>
                 <p>{introduction}</p>
